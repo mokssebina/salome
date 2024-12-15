2d162c189501ae9c8483213a5bf811a33ab816b2
mokssebina <sebina.moks@gmail.com>
2024-12-15 23:45:39 +0200

    header fixes

--- src/Components/Header.jsx
+++ src/Components/Header.jsx
@@ -5,7 +5,7 @@ import { MoonIcon, SunIcon } from '@heroicons/react/24/outline'
 
 const Header = ({day, setDay}) => {
   return (
-    <div className={`relative w-full md:w-[600px] h-16 mx-auto flex flex-row ${day? 'bg-[#cee5f0]' : 'bg-[#283038]'} p-1 rounded-lg`}>
+    <div className={`relative w-11/12 md:w-[600px] h-16 mx-auto flex flex-row ${day? 'bg-[#cee5f0]' : 'bg-[#283038]'} p-1 rounded-lg`}>
         <div style={{marginLeft: '20%'}} className={`header-logo w-3/5 h-full ${day? 'text-gray-900' : 'text-gray-50'} py-1 mx auto items-center text-center`}>
           <p className='relative text-3xl'>happy birthday</p>
         </div>
